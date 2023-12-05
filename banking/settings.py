392ea0d35bc727ec6989010ef272f24fe252a8fd
ZlayaMorda <tarus01@mail.ru>
2023-12-05 09:43:06 +0100

    fex changing in selects and static import

--- banking/settings.py
+++ banking/settings.py
@@ -131,9 +131,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 STATIC_URL = "static/"
 
