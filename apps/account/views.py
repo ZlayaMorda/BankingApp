8695a4a7d75217303b21fe81198e81d57491e76a
ZlayaMorda <tarus01@mail.ru>
2023-12-09 09:03:32 +0100

    Add account detail view

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from apps.account.services.account_service import AccountService
 
-# Create your views here.
+
+class AccountDetailView(View):
+    template_name = "account.html"
+    service = AccountService()
+
+    def get(self, request, pk):
+        context = {}
+        account = self.service.retrieve_account_by_pk(pk=pk)
+        if account:
+            context['account'] = self.service.get_account_context(account)
+        return render(request, template_name=self.template_name, context=context)
