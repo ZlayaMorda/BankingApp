803f7b7cad602c9b08873cd9dd7b2b2f48065571
ZlayaMorda <tarus01@mail.ru>
2023-12-09 09:04:48 +0100

    credit list

--- apps/credit/urls.py
+++ apps/credit/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("ajax/load-payments", views.CreditLoadPayment.as_view(), name="ajax_load_payments"),
     path("ajax/load-durations", views.CreditLoadDuration.as_view(), name="ajax_load_durations"),
     path("ajax/load-rate-percent", views.RatePercentView.as_view(), name="ajax_load_rate_percent"),
+    path("list/", views.CreditList.as_view(), name="credit_list"),
 ]
