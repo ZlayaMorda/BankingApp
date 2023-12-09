135ff07f8fde968993a4d2ca52a30c7bb7bda317
ZlayaMorda <tarus01@mail.ru>
2023-12-09 08:54:33 +0100

    Fix user

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,11 +1,12 @@
 import uuid
 from apps.users.managers import CustomUserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from utils.models import HistorizedModel
 
 
-class CustomUser(AbstractUser, HistorizedModel):
+class CustomUser(AbstractBaseUser, PermissionsMixin, HistorizedModel):
     class Roles(models.TextChoices):
         USER = "user"
         ADMIN = "admin"
@@ -13,14 +14,17 @@ class CustomUser(AbstractUser, HistorizedModel):
     user_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
-    passport_identifier = models.CharField(max_length=14)
+    passport_identifier = models.CharField(max_length=14, unique=True)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=5, choices=Roles.choices)
     is_deleted = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
-    REQUIRED_FIELDS = ["email"]
+    USERNAME_FIELD = 'passport_identifier'
+
+    REQUIRED_FIELDS = ["email", "first_name", "last_name"]
 
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.user_uuid
+        return str(self.user_uuid)
