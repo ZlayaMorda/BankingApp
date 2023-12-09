ebac37e3590944a164755248285e3562959a17de
ZlayaMorda <tarus01@mail.ru>
2023-12-09 09:08:58 +0100

    not valid transfer token

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("list/", views.AccountListView.as_view(), name="account_list"),
     path("transfer/<uuid:pk>/", views.AccountTransferView.as_view(), name="account_transfer"),
     path("create/", views.AccountCreateView.as_view(), name="account_create"),
-    path("delete/<uuid:pk>/", views.AccountDeleteView.as_view(), name="account_delete")
+    path("delete/<uuid:pk>/", views.AccountDeleteView.as_view(), name="account_delete"),
+    path("change-for-token/<uuid:pk>/", views.AccountTokenView.as_view(), name="change_for_token"),
 ]
