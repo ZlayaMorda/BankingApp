89a81b207b6990c48571e0e729aea15b839c251b
ZlayaMorda <tarus01@mail.ru>
2023-12-09 08:54:33 +0100

    Add migrations

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+USER_MODEL = get_user_model()
+
+
+CURRENCY_CHOICES = (
+    ("BYN", "Belarusian Rubles"),
+    ("USD", "United States Dollar"),
+    ("EUR", "Euro"),
+)
+
+
+class Account(models.Model):
+    account_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(USER_MODEL, on_delete=models.CASCADE, null=False, blank=False, related_name='accounts')
+    currency = models.CharField(max_length=64, choices=CURRENCY_CHOICES, default="BYN")
+    amount = models.DecimalField(max_digits=14, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
